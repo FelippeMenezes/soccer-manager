c3959180b382667b83a3fb71f4c3159c13dbe48c
Felippe Santana <42714571+FelippeMenezes@users.noreply.github.com>
2025-07-24 17:33:33 -0300

    change close flashes action

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // app/javascript/application.js
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Este código espera o Turbo carregar a página e então adiciona a funcionalidade.
+document.addEventListener("turbo:load", () => {
+  // Encontra todos os alertas que podem ser fechados.
+  const alerts = document.querySelectorAll('.alert-dismissible');
+  
+  alerts.forEach((alert) => {
+    // Para cada alerta, cria uma nova instância do componente Alert do Bootstrap.
+    // Isso garante que o JavaScript do Bootstrap saiba sobre este elemento.
+    new bootstrap.Alert(alert);
+  });
+});
